2e1f3b96993f45eb2b86e90bc8d1f3af76ccc048
korben.dong <korben.dong@sifive.com>
2019-07-24 10:04:44 +0800

    nvdal axi4 slave --> sbar 256bit OK

--- src/main/scala/unleashed/IOFPGADesign.scala
+++ src/main/scala/unleashed/IOFPGADesign.scala
@@ -133,6 +133,8 @@ class IOFPGADesign()(implicit p: Parameters) extends LazyModule with BindingScop
     FlipRendering { implicit p => mbar.node := TLFIFOFixer() } := TLWidthWidget(8) := nvdla.crossTLOut(nvdla.dbb_tl_node)
 
     nvdla.crossTLIn(nvdla.cfg_tl2axi4slv_node) := sbar.node
+    // nvdla.crossTLIn(nvdla.cfg_tl2axi4slv_node) := TLFilter(filter) := TLBuffer() := mbar.node
+    // nvdla.cfg_tl2axi4slv_node := TLBuffer() := mbar.node
 
 
     nvdla.crossTLIn(nvdla.cfg_tl_node := nvdla { TLFragmenter(4, 64) := TLWidthWidget(8) }) := sbar.node
